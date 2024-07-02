6bbe7fb212389e746ae49b25ea3a6e40fc570139
Quin Lynch <49576606+quinchs@users.noreply.github.com>
2024-07-02 02:06:48 -0300

    fix crash on no tabs

--- src/tabs/tabsManager.ts
+++ src/tabs/tabsManager.ts
@@ -34,26 +34,8 @@ export const isUriAtTab = (uri: string, tab: Tab) => {
 }
 
 export const getOpenTabs = (): Tab[] => {
-    // return [
-    //     {
-    //         name: "Liege",
-    //         channelId: "692174723341221958",
-    //         userId: "619241308912877609"
-    //     },
-    //     {
-    //         name: "general",
-    //         channelId: "732297728826277942",
-    //         guildId: "732297728826277939"
-    //     },
-    //     {
-    //         name: "android",
-    //         channelId: "732297837953679412",
-    //         guildId: "732297728826277939"
-    //     },
-    //    
-    // ]
 
-    return BdApi.Data.load("quinchs-discord", "open-tabs") as Tab[]
+    return BdApi.Data.load("quinchs-discord", "open-tabs") ?? [] as Tab[]
 }
 
 export const saveTabsState = (tabs: Tab[]) => {
