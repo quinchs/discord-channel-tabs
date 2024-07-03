85fcc3ca925f6b8d7cdd466d311631d46e85a2b2
Quin Lynch <quin@edgedb.com>
2024-07-03 00:48:09 -0300

    GDM context

--- src/injector.ts
+++ src/injector.ts
@@ -1,8 +1,6 @@
 ﻿import {createElement, Fragment} from "react";
 import {TabArea} from "./tabs/tabArea";
 import Plugin from "./index";
-import {Tab} from "./tabs/tabsManager";
-import {byModuleStrings} from "./utils/moduleSearchFilters";
 import {buildCloseTabsMenuItems, buildTabsContextMenuItems} from "./tabs/contextMenu";
 
 export const patchChatArea = (plugin: Plugin) => {
@@ -38,6 +36,7 @@ export const patchChannelMenu = (plugin: Plugin) => {
     
     const channelContext = BdApi.ContextMenu.patch("channel-context", callback);
     const userContext = BdApi.ContextMenu.patch("user-context", callback);
+    const groupContext = BdApi.ContextMenu.patch("gdm-context", callback);
     
     return () => {
         channelContext();
